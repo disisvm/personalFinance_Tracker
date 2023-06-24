73227d797ff625fac4673a6797a0f1a1c066f30d
Venkata Manish Katragadda <53395806+disisvm@users.noreply.github.com>
2023-06-24 16:23:23 -0400

    DB Connection

---
+++ db_connection.py
@@ -0,0 +1,21 @@
+from dotenv import load_dotenv, find_dotenv
+from pymongo import MongoClient
+
+
+def db_connection():
+    # ----------- DB Connection -------------
+
+    load_dotenv(find_dotenv())
+
+    password = os.environ.get("mongo_pwd")
+
+    # Create the connection string for MongoDB using the provided password
+    connection_string = f"mongodb+srv://kvenkatamanish:{password}@manish.9cdaxh3.mongodb.net/"
+
+    # Create a MongoClient object with the connection string
+    client = MongoClient(connection_string)
+
+    # Access the "personalFinance_Tracker" database
+    db = client.personalFinance_Tracker
+
+    return db
