ff52584b29ef4f3f9ca2c7fec845876b291f8b46
Venkata Manish Katragadda <53395806+disisvm@users.noreply.github.com>
2023-06-28 15:52:37 -0400

    income and expense tracking

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
 
 import db_connection as mongo
 
 app = Flask(__name__)
 
-
 # Collection variables
 users = mongo.db.users
+incomeCollection = mongo.db.income
+expenseCollection = mongo.db.expense
+
+
+def accountsCollection(username):
+
+    accountsList = list(mongo.db.accounts.find({"$or": [{"user": username}, {"default": True}]}))
+    values_list = [d['name'] for d in accountsList if 'name' in d]
+
+    return values_list
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -24,9 +33,9 @@ def login():
         user_details = users.find_one({"user": username, "password": password})
         try:
             if user_details['user'] == username and user_details['password'] == password:
-                return redirect('/dashboard')
+                return redirect('/dashboard/' + username)
         except TypeError:
-            return render_template('login.html', error_message=error_message)
+            return render_template('login.html')
 
         # Invalid credentials, show error message
         error_message = 'Invalid username or password.'
@@ -65,13 +74,50 @@ def register():
     return render_template('register.html')
 
 
-@app.route('/dashboard')
-def dashboard():
-    # Check if user is logged in
-    # If not logged in, redirect to login page
-    # Perform login check logic here
+@app.route('/income/<username>', methods=['GET', 'POST'])
+def add_income(username):
+    if request.method == 'POST':
+        amount = float(request.form['amount'])
+        description = request.form['description']
+        account = request.form['account']
 
-    return render_template('dashboard.html')
+        # Insert income record into the database
+        incomeCollection.insert_one(
+            {'user': username, 'amount': amount, 'description': description, 'account': account})
+
+        return redirect(url_for('dashboard', username=username))
+
+    return render_template('income.html', accounts=accountsCollection(username), username=username)
+
+
+@app.route('/expenses/<username>', methods=['GET', 'POST'])
+def add_expense(username):
+    if request.method == 'POST':
+        amount = float(request.form['amount'])
+        description = request.form['description']
+        account = request.form['account']
+
+        # Insert expense record into the database
+        expenseCollection.insert_one(
+            {'user': username, 'amount': amount, 'description': description, 'account': account})
+
+        return redirect(url_for('dashboard', username=username))
+
+    return render_template('expenses.html', accounts=accountsCollection(username), username=username)
+
+
+@app.route('/dashboard/<username>')
+def dashboard(username):
+    # Retrieve user's financial information from the database
+    # Replace the placeholders below with the actual data retrieval logic
+    account_balances = {'Savings': 5000, 'Checking': 2500, 'Investments': 10000}
+
+    # Retrieve income and expense records from the database for the specific user
+    income_records = list(incomeCollection.find({'username': username}))
+    expense_records = list(expenseCollection.find({'username': username}))
+
+    return render_template('dashboard.html', username=username, account_balances=account_balances,
+                           income_records=income_records, expense_records=expense_records)
 
 
 if __name__ == '__main__':
--- dashboard.py
+++
@@ -1,4 +0,0 @@
-from flask import Flask
-import db_connection as mongo
-
-print("Hello world")
