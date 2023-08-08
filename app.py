c3baa97354e68881309bf26287b8bca9f833f1d5
Venkata Manish Katragadda <53395806+disisvm@users.noreply.github.com>
2023-08-07 20:30:34 -0400

    Commit

--- app.py
+++ app.py
@@ -1,26 +1,31 @@
 import csv
+import os
 from datetime import datetime, date
-from io import BytesIO, StringIO
+from io import StringIO
 
-from flask import Flask, send_file, Response
+import matplotlib
+from flask import Flask, Response
 from flask import request, redirect, url_for, render_template
-from reportlab.lib.pagesizes import letter
-from reportlab.pdfgen import canvas
 
 import db_connection as mongo
+import matplotlib.pyplot as plt
+
+matplotlib.use('Agg')
 
 app = Flask(__name__)
 
 # Collection variables
 users = mongo.db.users
 transactionsCollection = mongo.db.transactions
+accountsCollection = mongo.db.accounts
 incomeCategoryCollection = mongo.db.incomeCategory
 expenseCategoryCollection = mongo.db.expenseCategory
 goalCollection = mongo.db.goals
+settingsCollection = mongo.db.settings
 
 
 def accounts_list(username):
-    accountsList = list(mongo.db.accounts.find({"$or": [{"user": username}, {"default": True}]}))
+    accountsList = list(accountsCollection.find({"$or": [{"user": username}, {"default": True}]}))
     values_list = [d['name'] for d in accountsList if 'name' in d]
 
     return values_list
@@ -38,6 +43,13 @@ def expense_category(username):
     values_list = [d['name'] for d in expense_categoryList if 'name' in d]
 
     return values_list
+
+
+def budget(username):
+    settingsList = settingsCollection.find_one({"user": username})
+    value = settingsList.get("budget")
+
+    return value
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -110,7 +122,6 @@ def add_income(username):
                                            'description': description,
                                            'account': account,
                                            'income_category': category,
-                                           'expense_category': '',
                                            'transaction_type': 'income',
                                            'date': datetime.now()})
 
@@ -133,7 +144,6 @@ def add_expense(username):
                                            'amount': amount,
                                            'description': description,
                                            'account': account,
-                                           'income_category': '',
                                            'expense_category': category,
                                            'transaction_type': 'expense',
                                            'date': datetime.now()})
@@ -243,6 +253,120 @@ def generate_csv(records):
     csv_data = csv_buffer.getvalue()
 
     return csv_data
+
+
+@app.route('/transaction_history/<username>', methods=['GET'])
+def transaction_history(username):
+    search = request.args.get('search', '')
+    filter_type = request.args.get('filter_type', '')
+    filter_account = request.args.get('filter_account', '')
+    filter_income_category = request.args.get('filter_income_category', '')
+    filter_expense_category = request.args.get('filter_expense_category', '')
+    min_amount = request.args.get('min_amount', '')
+    max_amount = request.args.get('max_amount', '')
+    start_date = request.args.get('start_date', '')
+    end_date = request.args.get('end_date', '')
+    sort = request.args.get('sort', '')
+
+    query = {
+        'user': username,
+        '$or': [
+            {'description': {'$regex': search, '$options': 'i'}},
+            {'account': {'$regex': search, '$options': 'i'}},
+            {'income_category': {'$regex': search, '$options': 'i'}},
+            {'expense_category': {'$regex': search, '$options': 'i'}}
+        ]
+    }
+
+    if filter_type:
+        query['transaction_type'] = filter_type
+
+    if filter_account:
+        query['account'] = filter_account
+
+    if filter_income_category:
+        query['income_category'] = filter_income_category
+
+    if filter_expense_category:
+        query['expense_category'] = filter_expense_category
+
+    if min_amount:
+        query['amount'] = {'$gte': float(min_amount)}
+
+    if max_amount:
+        query['amount']['$lte'] = float(max_amount)
+
+    if start_date:
+        query['date']['$gte'] = datetime.strptime(start_date, '%Y-%m-%d')
+
+    if end_date:
+        query['date']['$lte'] = datetime.strptime(end_date, '%Y-%m-%d')
+
+    sort_query = []
+    if sort == 'date_asc':
+        sort_query.append(('date', 1))
+    elif sort == 'date_desc':
+        sort_query.append(('date', -1))
+
+    transactions = transactionsCollection.find(query)
+    if sort_query:
+        transactions = transactions.sort(sort_query)
+
+    income_categories = income_category(username)
+    expense_categories = expense_category(username)
+    accounts = accounts_list(username)
+
+    return render_template('transaction_history.html', username=username, transactions=transactions,
+                           search=search, filter_type=filter_type, filter_account=filter_account,
+                           filter_income_category=filter_income_category,
+                           filter_expense_category=filter_expense_category, min_amount=min_amount,
+                           max_amount=max_amount, start_date=start_date, end_date=end_date,
+                           sort=sort, income_categories=income_categories, expense_categories=expense_categories,
+                           accounts=accounts)
+
+
+@app.route('/budget_management/<username>')
+def budget_management(username):
+    # Get current month and year
+    today = datetime.today()
+    current_month = today.month
+    current_year = today.year
+
+    # Fetch budget
+    budget_value = budget(username)
+
+    # Fetch expenses for the current month
+    expense_records = transactionsCollection.find({
+        'user': username,
+        'transaction_type': 'expense',
+        'date': {'$gte': datetime.combine(datetime(current_year, current_month, 1), datetime.min.time()),
+                 '$lt': datetime.combine(datetime(current_year, current_month + 1, 1), datetime.min.time())}
+    })
+
+    # Calculate total expenses and remaining budget
+    total_expenses = sum([d['amount'] for d in expense_records if 'amount' in d])
+    remaining_budget = budget_value - total_expenses
+
+    # Create a pie chart
+    labels = ['Expenses', 'Remaining Budget']
+    values = [total_expenses, remaining_budget]
+    colors = ['#FF9999', '#66B2FF']
+    plt.pie(values, labels=labels, colors=colors, autopct='%1.1f%%', startangle=90)
+    plt.axis('equal')  # Equal aspect ratio ensures the pie chart is circular.
+
+    # Save the pie chart to the static directory
+    chart_filename = 'chart.png'
+    static_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static')
+    chart_path = os.path.join(static_dir, chart_filename)
+    plt.savefig(chart_path, dpi=1000)
+    plt.close()
+
+    return render_template('budget_management.html',
+                           username=username,
+                           budgets=budget_value,
+                           chart_path=chart_path,
+                           total_expenses=total_expenses,
+                           remaining_budget=remaining_budget)
 
 
 @app.route('/logout', methods=['POST', 'GET'])
